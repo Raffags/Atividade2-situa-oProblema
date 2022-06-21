f5de048f0c9c997b13f6b4393634eda3de04c758
raffael <raffa.silveira@gmail.com>
2022-06-21 14:01:12 -0300

    entrega trabalho 2

---
+++ AV2.js
@@ -0,0 +1,22 @@
+let rs= require("readline-sync")
+let lista= [];
+let quantidadelista=lista.length;
+
+
+for (i=0; i<5;i++){
+
+    let idade=rs.question("Qual a sua idade ? ");
+  
+    let idadepermit= idade>=18? "Cadastro permitido  " : "Somente maiores de 18 podem se cadastrar"
+    console.log (idadepermit);
+    if (idade < 18) {
+        console.log("nao permitido");
+        continue;
+      }
+
+let nome = rs.question("qual seu nome ?");
+lista.push(nome);}      
+
+
+console.log(lista);
+console.log(lista.length);
